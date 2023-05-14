4bf5afbe0109729e04405def1282eb8d9c5c8f21
Skitt <sapolu08@gmail.com>
2023-05-14 20:49:06 +0200

    Update index.js
    Optimizacion

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
-exports.green = function (parametro) {
- console.log("\x1b[32m", parametro);
-}
-exports.red = function (parametro) {
- console.log("\x1b[31m", parametro);
-}
-exports.yellow = function (parametro) {
- console.log("\x1b[33m", parametro);
-}
-exports.blue = function (parametro) {
- console.log("\x1b[34m", parametro);
-}
-exports.magenta = function (parametro) {
- console.log("\x1b[35m", parametro);
-}
-exports.cyan = function (parametro) {
- console.log("\x1b[36m", parametro);
-}
-exports.white = function (parametro) {
- console.log("\x1b[37m", parametro);
-}
-exports.black = function (parametro) {
- console.log("\x1b[30m", parametro);
-}
-exports.underscore = function (parametro) {
- console.log("\x1b[4m", parametro);
-}
-exports.bright = function (parametro) {
- console.log("\x1b[1m", parametro);
-}
-exports.hidden = function (parametro) {
- console.log("\x1b[8m", parametro);
-}
-exports.reverse = function (parametro) {
- console.log("\x1b[7m", parametro);
-}
-exports.dim = function (parametro) {
- console.log("\x1b[2m", parametro);
-}
+const colors = {
+  green: (parametro) => console.log("\x1b[32m", parametro),
+  red: (parametro) => console.log("\x1b[31m", parametro),
+  yellow: (parametro) => console.log("\x1b[33m", parametro),
+  blue: (parametro) => console.log("\x1b[34m", parametro),
+  magenta: (parametro) => console.log("\x1b[35m", parametro),
+  cyan: (parametro) => console.log("\x1b[36m", parametro),
+  white: (parametro) => console.log("\x1b[37m", parametro),
+  black: (parametro) => console.log("\x1b[30m", parametro),
+  underscore: (parametro) => console.log("\x1b[4m", parametro),
+  bright: (parametro) => console.log("\x1b[1m", parametro),
+  hidden: (parametro) => console.log("\x1b[8m", parametro),
+  reverse: (parametro) => console.log("\x1b[7m", parametro),
+  dim: (parametro) => console.log("\x1b[2m", parametro)
+};
 
+module.exports = colors;
